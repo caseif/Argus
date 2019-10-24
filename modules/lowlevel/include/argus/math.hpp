889127285360aa7ac6b8c02bb29bdc0ab5cc9e27
Max Roncace <me@caseif.net>
2019-10-24 01:18:03 -0400

    Add LICENSE and generate license headers

--- modules/lowlevel/include/argus/math.hpp
+++ modules/lowlevel/include/argus/math.hpp
@@ -1,3 +1,12 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019, Max Roncace <mproncace@gmail.com>
+ *
+ * This software is made available under the MIT license. You should have
+ * received a copy of the full license text with this software. If not, the
+ * license text may be accessed at https://opensource.org/licenses/MIT.
+ */
+
 #pragma once
 
 #include <type_traits>
--- modules/renderer/include/internal/expand_dong.hpp
+++ modules/renderer/include/internal/expand_dong.hpp
@@ -1,3 +1,12 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019, Max Roncace <mproncace@gmail.com>
+ *
+ * This software is made available under the MIT license. You should have
+ * received a copy of the full license text with this software. If not, the
+ * license text may be accessed at https://opensource.org/licenses/MIT.
+ */
+
 // This little INDIRECT_EXPAND macro is necessary because MSVC is awful.
 //
 // For some reason known only to a few engineers and God, MSVC expands
