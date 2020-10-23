6fda397e45add497e499db954ca36fb668516e34
Max Roncace <me@caseif.net>
2020-10-22 22:38:01 -0400

    Clear framebuffers prior to rendering

--- modules/render_opengl/src/gl_renderer.cpp
+++ modules/render_opengl/src/gl_renderer.cpp
@@ -662,7 +662,7 @@ namespace argus {
 
         // clear framebuffer
         glClearColor(0.0, 0.0, 0.0, 0.0);
-        //glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         if (layer_state.frame_texture == 0 || renderer.get_window().pimpl->dirty_resolution) {
              if (layer_state.frame_texture != 0) {
