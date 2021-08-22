d3538897dcf15f62be22b4239b926a0391cd591c
Max Roncace <me@caseif.net>
2021-08-22 01:35:12 -0400

    lowlevel: Add thread pool interface

---
+++ modules/lowlevel/include/argus/lowlevel/threading/thread_pool.hpp
@@ -0,0 +1,121 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019-2021, Max Roncace <mproncace@gmail.com>
+ *
+ * This software is made available under the MIT license. You should have
+ * received a copy of the full license text with this software. If not, the
+ * license text may be accessed at https://opensource.org/licenses/MIT.
+ */
+
+#pragma once
+
+#include <functional>
+#include <future>
+
+#include <cstdint>
+
+namespace argus {
+    /**
+     * \brief For internal use only.
+     *
+     * \sa ThreadPool
+     */
+    struct pimpl_ThreadPool;
+
+    /**
+     * \brief A pool of threads to which tasks may be assigned.
+     *
+     * The pool will attempt to automatically balance the workload across the
+     * available threads to ensure efficiency.
+     */
+    class ThreadPool {
+        private:
+            pimpl_ThreadPool *pimpl;
+
+        public:
+            /**
+             * \brief Constructs a new ThreadPool with the thread count being
+             * initialized automatically based on the number of available
+             * logical cores.
+             */
+            ThreadPool(void);
+
+            /**
+             * \brief Constructs a new ThreadPool with a fixed number of
+             *        threads.
+             *
+             * \remark Providing a fixed thread count is generally discouraged
+             *         unless you know what you're doing - the nullary
+             *         constructor is recommended for most use cases.
+             */
+            ThreadPool(uint16_t threads);
+
+            /**
+             * \brief Submits a new task to the ThreadPool, passing the extra
+             *        parameter through to the provided callback.
+             *
+             * The pointer returned by the callback will be passed back through
+             * the std::future returned by this function.
+             *
+             * \param task The callback which the ThreadPool will invoke to
+             *        complete the task.
+             * \param param A parameter which will be passed through to the
+             *        callback.
+             * \return A std::future representing the result of the task
+             *         execution.
+             */
+            std::future<void*> submit(std::function<void*(void*)> task, void *param);
+
+            /**
+             * \brief Submits a new task to the ThreadPool.
+             *
+             * The pointer returned by the callback will be passed back through
+             * the std::future returned by this function.
+             *
+             * \param task The callback which the ThreadPool will invoke to
+             *        complete the task.
+             * \return A std::future representing the result of the task
+             *         execution.
+             */
+            std::future<void*> submit(std::function<void*(void)> task);
+
+            /**
+             * \brief Submits a new task to the ThreadPool, passing the extra
+             *        parameter through to the provided callback.
+             *
+             * The pointer returned by the callback will be passed back through
+             * the std::future returned by this function.
+             *
+             * \tparam R The type returned by the callback function.
+             * \tparam P The type of the parameter accepted by the callback
+             *         function.
+             * \param task The callback which the ThreadPool will invoke to
+             *        complete the task.
+             * \param param A parameter which will be passed through to the
+             *        callback.
+             * \return A std::future representing the result of the task
+             *         execution.
+             */
+            template <typename R, typename P>
+            std::future<R*> submit(std::function<R*(P*)> task, P *param) {
+                return static_cast<R*>(this->submit(task, static_cast<void*>(param)));
+            }
+
+            /**
+             * \brief Submits a new task to the ThreadPool.
+             *
+             * The pointer returned by the callback will be passed back through
+             * the std::future returned by this function.
+             *
+             * \tparam R The type returned by the callback function.
+             * \param task The callback which the ThreadPool will invoke to
+             *        complete the task.
+             * \return A std::future representing the result of the task
+             *         execution.
+             */
+            template <typename R>
+            std::future<R*> submit(std::function<R*(void)> task) {
+                return static_cast<R*>(this->submit(task));
+            }
+    };
+}
---
+++ modules/lowlevel/include/internal/lowlevel/pimpl/thread_pool.cpp
@@ -0,0 +1,16 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019-2021, Max Roncace <mproncace@gmail.com>
+ *
+ * This software is made available under the MIT license. You should have
+ * received a copy of the full license text with this software. If not, the
+ * license text may be accessed at https://opensource.org/licenses/MIT.
+ */
+
+#pragma once
+
+namespace argus {
+    struct pimpl_ThreadPool {
+        //TODO
+    };
+}
