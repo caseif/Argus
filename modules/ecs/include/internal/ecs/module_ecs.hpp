3d7541e563ebf25b60924d99d7da6b92e0ce97f4
Max Roncace <me@caseif.net>
2021-09-20 09:14:58 -0400

    Track missing files

---
+++ modules/ecs/include/internal/ecs/module_ecs.hpp
@@ -0,0 +1,18 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019-2021, Max Roncace <mproncace@gmail.com>
+ *
+ * This software is made available under the MIT license. You should have
+ * received a copy of the full license text with this software. If not, the
+ * license text may be accessed at https://opensource.org/licenses/MIT.
+ */
+
+#pragma once
+
+#include "argus/core/module.hpp"
+
+namespace argus {
+    void init_module_ecs(void);
+
+    void update_lifecycle_ecs(LifecycleStage stage);
+}
