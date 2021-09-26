da8e101c925899263548c8cece844535f12ca762
Max Roncace <me@caseif.net>
2021-09-25 22:37:31 -0400

    core: Perform topological sort on dynamic modules after loading them

--- engine/static/core/src/module.cpp
+++ engine/static/core/src/module.cpp
@@ -32,6 +32,7 @@
 #include <map>
 #include <set>
 #include <sstream>
+#include <stack>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -143,6 +144,88 @@ namespace argus {
         return target;
     }
 
+    template <typename T>
+    static std::vector<std::string> _topo_sort(const std::vector<T> nodes,
+            const std::vector<std::pair<T, T>> edges) {
+        std::vector<T> sorted_nodes;
+        std::deque<T> start_nodes;
+        std::vector<std::pair<T, T>> remaining_edges(edges);
+
+        for (auto &node : nodes) {
+            start_nodes.push_back(node);
+        }
+
+        for (auto &edge : remaining_edges) {
+            start_nodes.erase(std::find(start_nodes.begin(), start_nodes.end(), edge.second));
+        }
+
+        while (!start_nodes.empty()) {
+            auto &cur_node = start_nodes.front();
+            start_nodes.pop_front();
+            if (std::count(sorted_nodes.begin(), sorted_nodes.end(), cur_node) == 0) {
+                sorted_nodes.push_back(cur_node);
+            }
+
+            std::vector<std::pair<T, T>> remove_edges;
+            for (auto &cur_edge : remaining_edges) {
+                auto &dest_node = cur_edge.second;
+                if (cur_edge.first != cur_node) {
+                    continue;
+                }
+
+                remove_edges.push_back(cur_edge);
+
+                bool has_incoming_edges = false;
+                for (auto &check_edge : remaining_edges) {
+                    if (check_edge != cur_edge && check_edge.second == dest_node) {
+                        has_incoming_edges = true;
+                        break;
+                    }
+                }
+
+                if (!has_incoming_edges) {
+                    start_nodes.push_back(dest_node);
+                }
+            }
+
+            for (auto &edge : remove_edges) {
+                remaining_edges.erase(std::find(remaining_edges.begin(), remaining_edges.end(), edge));
+            }
+        }
+
+        if (!remaining_edges.empty()) {
+            throw std::invalid_argument("Graph contains cycles");
+        }
+
+        return sorted_nodes;
+    }
+
+    static std::vector<DynamicModule> _topo_sort_modules(const std::map<const std::string, DynamicModule> module_map) {
+        std::vector<std::string> module_ids;
+        std::vector<std::pair<std::string, std::string>> edges;
+
+        for (auto &mod_pair : module_map) {
+            module_ids.push_back(mod_pair.first);
+            for (auto &dep : mod_pair.second.dependencies) {
+                if (module_map.find(dep) != module_map.end()) {
+                    edges.push_back({dep, mod_pair.first});
+                }
+            }
+        }
+
+        std::vector<DynamicModule> sorted_modules;
+        try {
+            auto sorted_ids = _topo_sort(module_ids, edges);
+            for (auto &id : sorted_ids) {
+                sorted_modules.push_back(module_map.find(id)->second);
+            }
+        } catch (std::invalid_argument) {
+            _ARGUS_FATAL("Circular dependency detected in dynamic modules, cannot proceed.\n");
+        }
+
+        return sorted_modules;
+    }
+
     void load_dynamic_modules(void) {
         //TODO: we really shouldn't be loading modules that aren't explicitly requested
         auto present_modules = get_present_dynamic_modules();
@@ -172,9 +255,9 @@ namespace argus {
             g_dyn_module_registrations.find(module_id)->second.handle = handle;
         }
 
-        //TODO: do topo. sort before populating list
-        for (const auto &mod : g_dyn_module_registrations) {
-            g_sorted_dyn_modules.push_back(mod.second);
+        auto sorted_modules = _topo_sort_modules(g_dyn_module_registrations);
+        for (auto &mod : sorted_modules) {
+            g_sorted_dyn_modules.push_back(mod);
         }
     }
 
@@ -200,10 +283,12 @@ namespace argus {
             auto *found_static = std::find_if(g_static_modules.begin(), g_static_modules.end(),
                 [module_id](auto &sm) { return sm.id == module_id; });
             if (found_static != g_static_modules.end()) {
+                all_modules.insert({found_static->id});
                 all_modules.insert(found_static->dependencies.begin(), found_static->dependencies.end());
             } else {
                 auto found_dyn = g_dyn_module_registrations.find(module_id);
                 if (found_dyn != g_dyn_module_registrations.end()) {
+                    all_modules.insert({found_dyn->first});
                     auto trans_deps = _resolve_transitive_dependencies(found_dyn->second.dependencies, { module_id });
                     all_modules.insert(trans_deps.begin(), trans_deps.end());
                 } else {
@@ -246,7 +331,7 @@ namespace argus {
 
         g_dyn_module_registrations.insert({ id, std::move(mod) });
 
-        _ARGUS_INFO("Registered module %s\n", id.c_str());
+        _ARGUS_INFO("Registered dynamic module %s\n", id.c_str());
     }
 
     static void _enable_dynamic_module(const std::string &module_id, const std::vector<std::string> &dependent_chain) {
