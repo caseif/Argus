10d6b689943333f5facfdc8394b1bed9cdd36c83
Max Roncace <me@caseif.net>
2022-05-03 23:01:30 -0400

    core: Add basic client config implementation

---
+++ engine/static/core/src/client_config.cpp
@@ -0,0 +1,40 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019-2022, Max Roncace <mproncace@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "argus/core/client_config.hpp"
+#include "internal/core/client_config.hpp"
+
+namespace argus {
+    static ClientConfig g_client_config;
+
+    void set_client_id(const std::string &id) {
+        g_client_config.id = id;
+    }
+
+    void set_client_name(const std::string &name) {
+        g_client_config.name = name;
+    }
+
+    void set_client_version(const std::string &version) {
+        g_client_config.version = version;
+    }
+
+    ClientConfig &get_client_config(void) {
+        return g_client_config;
+    }
+}
