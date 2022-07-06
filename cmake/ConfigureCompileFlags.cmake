c63f0c90f87d804a298b3b97cfdcd1052fd6d1f7
Max Roncace <me@caseif.net>
2022-07-06 00:24:45 -0400

    buildscript: Don't set C++ compile flags for C targets

--- cmake/ConfigureCompileFlags.cmake
+++ cmake/ConfigureCompileFlags.cmake
@@ -2,12 +2,19 @@ function(_argus_set_compile_flags TARGET)
   if(MSVC)
     target_compile_options("/W4" "/wd4996")
   else()
-    target_compile_options("${TARGET}" PUBLIC "-Werror" "-Wall" "-Wextra" "-Wold-style-cast" "-Woverloaded-virtual"
-                           "-Wuninitialized" "-Wconversion" "-Wno-error=conversion"
-                           "-Wno-error=sign-conversion")
+    target_compile_options("${TARGET}" PUBLIC
+      "-Werror"
+      "-Wall"
+      "-Wextra"
+      "$<$<COMPILE_LANGUAGE:CXX>:-Wold-style-cast>"
+      "$<$<COMPILE_LANGUAGE:CXX>:-Woverloaded-virtual>"
+      "-Wuninitialized"
+      "-Wconversion"
+      "-Wno-error=conversion"
+      "-Wno-error=sign-conversion")
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang"
        OR (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 10))
-      target_compile_options("${TARGET}" PUBLIC "-Werror=mismatched-tags")
+      target_compile_options("${TARGET}" PUBLIC "$<$<COMPILE_LANGUAGE:CXX>:-Werror=mismatched-tags>")
     endif()
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
@@ -16,8 +23,11 @@ function(_argus_set_compile_flags TARGET)
       #   - anonymous structs, since there's no good standard alternative
       #   - zero-length arrays, since again there's no good standard alternative
       # we also only enable pedantic errors for Clang because GCC apparently lacks the ability to configure exceptions
-      target_compile_options("${TARGET}" PUBLIC "-Wpedantic" "-Wno-gnu-zero-variadic-macro-arguments"
-                             "-Wno-gnu-anonymous-struct" "-Wno-zero-length-array")
+      target_compile_features("${TARGET}" PUBLIC
+        "-Wpedantic"
+        "-Wno-gnu-zero-variadic-macro-arguments"
+        "-Wno-gnu-anonymous-struct"
+        "-Wno-zero-length-array")
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
       target_compile_options("${TARGET}" PUBLIC "-Wno-variadic-macros")
     endif()
