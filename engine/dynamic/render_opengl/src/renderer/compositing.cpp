14d045cf1f134135d027669bd6c513fab5b15320
Max Roncace <me@caseif.net>
2022-11-05 00:41:43 -0400

    render_opengl: Restore proper clear color for framebuffer and screen rendering

--- engine/dynamic/render_opengl/src/renderer/compositing.cpp
+++ engine/dynamic/render_opengl/src/renderer/compositing.cpp
@@ -208,7 +208,7 @@ namespace argus {
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, viewport_state.front_fb);
 
         // clear framebuffer
-        glClearColor(0.5, 0.0, 0.0, 1.0);
+        glClearColor(0.0, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glViewport(
