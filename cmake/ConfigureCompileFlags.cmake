24e7a8e7cffd315a96d9e3aecab577895115d5bb
Max Roncace <me@caseif.net>
2022-12-15 00:11:37 -0500

    buildscript: Use target_(compile|link)_options for all build flags

--- cmake/ConfigureCompileFlags.cmake
+++ cmake/ConfigureCompileFlags.cmake
@@ -1,6 +1,14 @@
 function(_argus_set_compile_flags TARGET)
   if(MSVC)
-    target_compile_options("${TARGET}" PUBLIC "/W4" "/wd4996")
+    target_compile_options("${TARGET}" PUBLIC "/W4" "/wd4996"
+        "$<$<CONFIG:Debug>:/Od>")
+    if(MSVC_VERSION LESS 1911)
+      target_compile_options("${TARGET}" PUBLIC "$<$<CONFIG:Debug>:/DEBUG:FULL>")
+    else()
+      target_compile_options("${TARGET}" PUBLIC "$<$<CONFIG:Debug>:/DEBUG>")
+    endif()
+
+    target_compile_options("${TARGET}" PUBLIC "$<$<CONFIG:Release>:/O2>")
   else()
     target_compile_options("${TARGET}" PUBLIC
       "-Werror"
@@ -19,6 +27,11 @@ function(_argus_set_compile_flags TARGET)
       target_compile_options("${TARGET}" PUBLIC "$<$<COMPILE_LANGUAGE:CXX>:-Werror=mismatched-tags>")
     endif()
 
+    target_compile_options("${TARGET}" PUBLIC "$<$<CONFIG:Debug>:-gdwarf-4>")
+    target_link_options("${TARGET}" PUBLIC "$<$<CONFIG:Debug>:-gdwarf-4>")
+
+    target_compile_options("${TARGET}" PUBLIC "$<$<CONFIG:Release>:-O3>")
+
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
       # enable pedantic errors, but with a few exceptions:
       #   - pasting , and __va_args__ so we can have a nice assert macro
@@ -30,6 +43,16 @@ function(_argus_set_compile_flags TARGET)
         "-Wno-gnu-zero-variadic-macro-arguments"
         "-Wno-gnu-anonymous-struct"
         "-Wno-zero-length-array")
+      if("${USE_ASAN}")
+        target_compile_options("${TARGET}" PUBLIC
+          "-fsanitize=address"
+          "-fno-omit-frame-pointer"
+          "-mllvm"
+          "-asan-use-private-alias=1")
+        target_link_options("${TARGET}" PUBLIC
+          "-fsanitize=address"
+          "-fno-omit-frame-pointer")
+      endif()
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
       target_compile_options("${TARGET}" PUBLIC "-Wno-variadic-macros")
     endif()
