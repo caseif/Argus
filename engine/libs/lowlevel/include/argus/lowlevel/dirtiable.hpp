25bcc955b35f841a78fd0b3ce280ea95f319fbd2
Max Roncace <me@caseif.net>
2023-01-14 15:42:30 -0500

    Update license headers

--- engine/libs/lowlevel/include/argus/lowlevel/dirtiable.hpp
+++ engine/libs/lowlevel/include/argus/lowlevel/dirtiable.hpp
@@ -1,6 +1,6 @@
 /*
  * This file is a part of Argus.
- * Copyright (c) 2019-2022, Max Roncace <mproncace@protonmail.com>
+ * Copyright (c) 2019-2023, Max Roncace <mproncace@protonmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
  
 #pragma once
 
