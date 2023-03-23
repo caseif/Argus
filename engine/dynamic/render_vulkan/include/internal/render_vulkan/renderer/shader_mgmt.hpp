89fcc9bb1c8664bb7a4a97bd8b5252e757f5f233
Max Roncace <me@caseif.net>
2023-03-22 22:02:15 -0400

    render_vulkan: Fix function names in shader_mgmt header

--- engine/dynamic/render_vulkan/include/internal/render_vulkan/renderer/shader_mgmt.hpp
+++ engine/dynamic/render_vulkan/include/internal/render_vulkan/renderer/shader_mgmt.hpp
@@ -32,10 +32,10 @@ namespace argus {
         ShaderReflectionInfo reflection;
     };
 
-    PreparedShaderSet link_program(VkDevice device,
+    PreparedShaderSet prepare_shaders(VkDevice device,
             std::initializer_list<std::pair<std::string, ShaderStage>> shader_uids);
 
-    PreparedShaderSet link_program(VkDevice device, std::vector<std::pair<std::string, ShaderStage>> shader_uids);
+    PreparedShaderSet prepare_shaders(VkDevice device, std::vector<std::pair<std::string, ShaderStage>> shader_uids);
 
     void destroy_shaders(VkDevice device, PreparedShaderSet shaders);
 }
