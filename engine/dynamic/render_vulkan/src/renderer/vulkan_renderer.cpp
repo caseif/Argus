aa89d33f89f6151a43fc90447ebf9fe701a88a28
Max Roncace <me@caseif.net>
2023-05-06 14:37:54 -0400

    render_vulkan: Try to fix MSVC compile errors again

--- engine/dynamic/render_vulkan/src/renderer/vulkan_renderer.cpp
+++ engine/dynamic/render_vulkan/src/renderer/vulkan_renderer.cpp
@@ -395,8 +395,8 @@ namespace argus {
         static std::chrono::nanoseconds draw_time;
         static std::chrono::nanoseconds composite_time;
 
-        std::chrono::system_clock::time_point timer_start;
-        std::chrono::system_clock::time_point timer_end;
+        std::chrono::high_resolution_clock ::time_point timer_start;
+        std::chrono::high_resolution_clock::time_point timer_end;
 
         if (std::chrono::high_resolution_clock::now() - last_print >= 10s && time_samples > 0) {
             Logger::default_logger().debug("Rebuild + draw + composite took %ld + %ld + %ld ns\n",
