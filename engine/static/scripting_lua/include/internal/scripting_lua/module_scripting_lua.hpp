d4f89fb2a1497baaf2b5ba930db26ae7b28c6237
Max Roncace <me@caseif.net>
2023-06-08 23:04:35 -0400

    scripting_lua: Add scripting_lua module

---
+++ engine/static/scripting_lua/include/internal/scripting_lua/module_scripting_lua.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "argus/core/module.hpp"
+
+namespace argus {
+    void update_lifecycle_scripting_lua(LifecycleStage stage);
+}
---
+++ engine/static/scripting_lua/src/module_scripting_lua.cpp
@@ -0,0 +1,10 @@
+#include "argus/core/module.hpp"
+
+#include "internal/scripting_lua/module_scripting_lua.hpp"
+
+namespace argus {
+    void update_lifecycle_scripting_lua(LifecycleStage stage) {
+        //TODO
+        UNUSED(stage);
+    }
+}
