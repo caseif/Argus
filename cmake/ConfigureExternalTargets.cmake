81b28c5b110f10a02cbebd0f933ea0654195c0f0
Max Roncace <me@caseif.net>
2023-11-09 23:32:04 -0500

    buildscript: Use standard variable naming for external libraries

--- cmake/ConfigureExternalTargets.cmake
+++ cmake/ConfigureExternalTargets.cmake
@@ -52,58 +52,83 @@ endif()
 # set relevant build variables so the dependencies can be discovered
 # note that we include the dirs for generated headers
 
-set(SDL2_LIBRARY "SDL2")
-set(SDL2_INCLUDE_DIR "${SDL2_SOURCE_DIR}/include")
-
-set(ZLIB_LIBRARY "zlib")
-set(ZLIB_INCLUDE_DIR "${ZLIB_SOURCE_DIR};${TMP_INCLUDE_DIR}/zlib")
-
-set(PNG_LIBRARY "png")
-set(PNG_INCLUDE_DIR "${PNG_SOURCE_DIR};${TMP_INCLUDE_DIR}/libpng")
-
-set(JSON_INCLUDE_DIR "${JSON_SOURCE_DIR}/include")
-
-set(ARP_LIBRARY_BASE "arp")
-set(ARP_LIBRARY "${ARP_LIBRARY_BASE};${ZLIB_LIBRARY}")
-set(ARP_INCLUDE_DIR "${ARP_SOURCE_DIR}/include")
-
-set(GLSLANG_LIBRARY "glslang;SPIRV")
-set(GLSLANG_INCLUDE_DIR "${GLSLANG_SOURCE_DIR}")
-
-set(SPIRV_CROSS_LIBRARY "spirv-cross-cpp")
-set(SPIRV_CROSS_INCLUDE_DIR "${SPIRV_CROSS_SOURCE_DIR}")
-
-set(LUA_LIBRARY "lua")
-set(LUA_INCLUDE_DIR "${LUA_SOURCE_DIR}")
-
-set(ANGELSCRIPT_LIBRARY "angelscript")
-set(ANGELSCRIPT_INCLUDE_DIR "${ANGELSCRIPT_SOURCE_DIR}/include;${ANGELSCRIPT_SOURCE_DIR}/../add_on")
-
-# set build variables for test dependencies
-set(CATCH2_LIBRARY "Catch2")
-set(CATCH2_INCLUDE_DIR "${CATCH2_SOURCE_DIR}/src")
-
 # add dependencies
+
+# SDL2
+set(SDL2_LIBRARY "SDL2")
+set(SDL2_LIBRARIES "${SDL2_LIBRARY}")
+set(SDL2_INCLUDE_DIRS "${SDL2_SOURCE_DIR}/include")
 add_subdirectory("${SDL2_SOURCE_DIR}")
+
+# zlib
+set(ZLIB_LIBRARY "zlib")
+set(ZLIB_LIBRARIES "${ZLIB_LIBRARY}")
+set(ZLIB_INCLUDE_DIRS "${ZLIB_SOURCE_DIR};${TMP_INCLUDE_DIR}/zlib")
+set(ZLIB_INCLUDE_DIR "${ZLIB_INCLUDE_DIRS}")
 add_subdirectory("${ZLIB_SOURCE_DIR}")
+
+# libpng
+set(PNG_LIBRARY "png")
+set(PNG_LIBRARIES "${PNG_LIBRARY}")
+set(PNG_INCLUDE_DIRS "${PNG_SOURCE_DIR};${TMP_INCLUDE_DIR}/libpng")
+set(PNG_INCLUDE_DIR "${PNG_INCLUDE_DIRS}")
+set(PNG_BUILD_ZLIB ON CACHE BOOL "" FORCE)
 add_subdirectory("${PNG_SOURCE_DIR}")
+
+# json
+set(JSON_INCLUDE_DIRS "${JSON_SOURCE_DIR}/include")
+set(JSON_INCLUDE_DIR "${JSON_INCLUDE_DIRS}")
+# header-only library, nothing to include here
+
+# libarp
+set(ARP_LIBRARY "arp")
+set(ARP_LIBRARIES "${ARP_LIBRARY};${ZLIB_LIBRARIES}")
+set(ARP_INCLUDE_DIRS "${ARP_SOURCE_DIR}/include")
+set(ARP_INCLUDE_DIR "${ARP_INCLUDE_DIRS}")
 add_subdirectory("${ARP_SOURCE_DIR}")
-add_subdirectory("${SPIRV_CROSS_SOURCE_DIR}")
-add_subdirectory("${LUA_BUILDSCRIPT_DIR}")
-#add_subdirectory("${ANGELSCRIPT_SOURCE_DIR}/projects/cmake")
 
-#_argus_append_source_files(${ANGELSCRIPT_LIBRARY} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptarray")
-#_argus_append_source_files(${ANGELSCRIPT_LIBRARY} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptbuilder")
-#_argus_append_source_files(${ANGELSCRIPT_LIBRARY} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptstdstring")
-
+# glslang
+set(GLSLANG_LIBRARY "glslang")
+set(GLSLANG_LIBRARIES "${GLSLANG_LIBRARY};SPIRV")
+set(GLSLANG_INCLUDE_DIRS "${GLSLANG_SOURCE_DIR}")
+set(GLSLANG_INCLUDE_DIR "${GLSLANG_INCLUDE_DIRS}")
 # we want to build this lib statically
 set(BUILD_SHARED_LIBS_SAVED "${BUILD_SHARED_LIBS}")
 set(BUILD_SHARED_LIBS OFF)
 add_subdirectory("${GLSLANG_SOURCE_DIR}")
 set(BUILD_SHARED_LIBS "${BUILD_SHARED_LIBS_SAVED}")
 
+# SPIRV-Cross
+set(SPIRV_CROSS_LIBRARY "spirv-cross-cpp")
+set(SPIRV_CROSS_LIBRARIES "${SPIRV_CROSS_LIBRARY}")
+set(SPIRV_CROSS_INCLUDE_DIRS "${SPIRV_CROSS_SOURCE_DIR}")
+set(SPIRV_CROSS_INCLUDE_DIR "${SPIRV_CROSS_INCLUDE_DIRS}")
+add_subdirectory("${SPIRV_CROSS_SOURCE_DIR}")
+
+# Lua
+set(LUA_LIBRARY "lua")
+set(LUA_LIBRARIES "${LUA_LIBRARY}")
+set(LUA_INCLUDE_DIRS "${LUA_SOURCE_DIR}")
+set(LUA_INCLUDE_DIR "${LUA_INCLUDE_DIR}")
+add_subdirectory("${LUA_BUILDSCRIPT_DIR}")
+
+# Angelscript
+set(ANGELSCRIPT_LIBRARY "angelscript")
+set(ANGELSCRIPT_LIBRARIES "${ANGELSCRIPT_LIBRARY}")
+set(ANGELSCRIPT_INCLUDE_DIRS "${ANGELSCRIPT_SOURCE_DIR}/include;${ANGELSCRIPT_SOURCE_DIR}/../add_on")
+set(ANGELSCRIPT_INCLUDE_DIR "${ANGELSCRIPT_INCLUDE_DIRS}")
+#add_subdirectory("${ANGELSCRIPT_SOURCE_DIR}/projects/cmake")
+#_argus_append_source_files(${ANGELSCRIPT_LIBRARIES} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptarray")
+#_argus_append_source_files(${ANGELSCRIPT_LIBRARIES} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptbuilder")
+#_argus_append_source_files(${ANGELSCRIPT_LIBRARIES} "${ANGELSCRIPT_SOURCE_DIR}/../add_on/scriptstdstring")
+
 # add test dependencies
 
+# set build variables for test dependencies
+set(CATCH2_LIBRARY "Catch2")
+set(CATCH2_LIBRARIES "${CATCH2_LIBRARY}")
+set(CATCH2_INCLUDE_DIRS "${CATCH2_SOURCE_DIR}/src")
+set(CATCH2_INCLUDE_DIR "${CATCH2_INCLUDE_DIRS}")
 # we want to build this lib statically
 set(BUILD_SHARED_LIBS_SAVED "${BUILD_SHARED_LIBS}")
 set(BUILD_SHARED_LIBS OFF)
