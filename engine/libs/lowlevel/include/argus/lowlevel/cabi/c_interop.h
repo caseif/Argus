695a3cf70bc2bed9712584d47bbdfe27f2e96b9d
Max Roncace <me@caseif.net>
2024-02-11 12:53:02 -0500

    lowlevel: Add C wrapper for std::vector<string>

---
+++ engine/libs/lowlevel/include/argus/lowlevel/cabi/c_interop.h
@@ -0,0 +1,38 @@
+/*
+ * This file is a part of Argus.
+ * Copyright (c) 2019-2023, Max Roncace <mproncace@protonmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+typedef void *StringArray;
+typedef const void *StringArrayConst;
+
+size_t string_array_get_count(StringArrayConst sa);
+
+const char *string_array_get_element(StringArrayConst sa, size_t index);
+
+void string_array_free(StringArray sa);
+
+#ifdef __cplusplus
+}
+#endif
