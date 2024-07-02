b224318964b7bf20b8a9cf36b16a2f10ad8fb0e2
Max Roncace <me@caseif.net>
2024-07-02 11:12:58 -0400

    test: Use specific Catch2 headers instead of catch_all.hpp

--- test/include/test_global.hpp
+++ test/include/test_global.hpp
@@ -23,7 +23,13 @@
 #pragma GCC diagnostic ignored "-Wparentheses"
 
 #define CATCH_CONFIG_MAIN
-#include "catch2/catch_all.hpp"
+#include "catch2/catch_template_test_macros.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/generators/catch_generators.hpp"
+#include "catch2/generators/catch_generators_adapters.hpp"
+#include "catch2/generators/catch_generators_random.hpp"
+#include "catch2/generators/catch_generators_range.hpp"
+#include "catch2/matchers/catch_matchers_floating_point.hpp"
 
 #pragma GCC diagnostic pop
 
