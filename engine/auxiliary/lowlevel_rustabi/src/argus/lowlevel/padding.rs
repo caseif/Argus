e22a50c8659a275b11f25db7810482ac32d1fc3b
Max Roncace <me@caseif.net>
2024-12-10 22:43:01 -0500

    lowlevel_rs: Add Debug to some types that were missing it

--- engine/auxiliary/lowlevel_rustabi/src/argus/lowlevel/padding.rs
+++ engine/auxiliary/lowlevel_rustabi/src/argus/lowlevel/padding.rs
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#[derive(Clone, Copy, Default)]
+#[derive(Clone, Copy, Debug, Default)]
 pub struct Padding {
     pub top: u32,
     pub bottom: u32,
